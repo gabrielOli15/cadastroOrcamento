198296cad00e7c7fba9c5d14cd0adb78a7f82300
Gabriel Oliveira <37242012+gabrielOli15@users.noreply.github.com>
2025-03-27 18:02:48 -0300

    Adiciona novos campos 'zx2_mod' e 'zx2_tpprod' à interface Parametro, expandindo a estrutura para melhor suporte a dados de produtos.

--- src/app/cadastra-parametros/shared/interface/parametro.ts
+++ src/app/cadastra-parametros/shared/interface/parametro.ts
@@ -2,6 +2,7 @@ export interface Parametro {
     zx2_cod: string;
     zx2_desc: string;
     zx2_vlprod: number;
+    zx2_mod: string;
     zx2_kit: string;
     zx2_cmc: number;
     zx2_cmt: number;
@@ -10,4 +11,5 @@ export interface Parametro {
     zx2_fmc: number;
     zx2_fmt: number;
     zx2_var: string;
+    zx2_tpprod: string;
 }
