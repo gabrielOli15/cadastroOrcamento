1a55d842b907e142f7ca7c2dd7a6d3af5567fef0
Gabriel Oliveira <37242012+gabrielOli15@users.noreply.github.com>
2025-03-28 09:36:17 -0300

    Auto-generated commit

---
+++ chunk-FF4B56LT.js
@@ -0,0 +1,1 @@
+var t=class{constructor(s={}){this.USER="TOTVS:t0tvs",this.URL="http://177.184.144.123:7200/rest",Object.assign(this,s)}};export{t as a};
---
+++ chunk-RP53FWOI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-DO4H65YQ.js";import"./chunk-63LJVA47.js";export{a as PainelComponent};
