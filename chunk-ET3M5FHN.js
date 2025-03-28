7cadc540d2a71716c0362536f4f723d13b97030f
Gabriel Oliveira <37242012+gabrielOli15@users.noreply.github.com>
2025-03-28 10:31:25 -0300

    Auto-generated commit

---
+++ chunk-ET3M5FHN.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-DYCP2L7K.js";import"./chunk-LGW2HRLS.js";export{a as PainelComponent};
--- chunk-RP53FWOI.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-DO4H65YQ.js";import"./chunk-63LJVA47.js";export{a as PainelComponent};
